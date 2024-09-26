75d07ae4acf305337edb26000916eb28cb47facc
logesh <logeshgopi.madasamy@gmail.com>
2024-09-26 12:24:39 +0530

    install tailwind and add daisy ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 // import './App.css'
 
+import Navbar from "./Components/Navbar";
+
 function App() {
   return (
-   <h1>Hello World</h1>
+    <>
+      <Navbar/>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
   );
 }
 
-export default App
+export default App;
