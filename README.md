18f3cfc279f77bbb612144e7a4ca59aea99d2eea
logesh <logeshgopi.madasamy@gmail.com>
2024-09-27 13:51:21 +0530

    Install axios and test Login api from backend

--- README.md
+++ README.md
@@ -10,5 +10,9 @@
   -Create browser router 
   -Create Login and profile page and render into body component
   -Create Footer Component and render into Body
+  -install axios 
+  -Install cors in backend and configure with Options
+  -And configure axios with credentials to get a token froma backend
+   
 
 
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,44 @@
-const Login = ()=>{
-    return (
-        <div>
-            Login page
+import axios from "axios";
+import { useState } from "react";
+
+const Login = () => {
+
+    const [emailID,seEmailId ]= useState("janani@gmail.com");
+    const [password,setPassword] = useState("Janani@003");
+
+    const handleLogin =async()=>{
+       try {
+           const res = await axios.post("http://localhost:3500/login",{emailID,password},
+            {withCredentials:true}
+           )
+       } catch (error) {
+         console.log(error);
+         
+       }
+        
+    }
+
+  return (
+    <div className="flex justify-center mt-[3%]">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Login </h2> 
+          <div>
+          <input type="text" value={emailID} onChange={(e)=>seEmailId(e.target.value)}
+           placeholder="Email " className="input input-bordered w-full max-w-xs my-2" />
+
+          <input type="text"  value={password} onChange={(e)=>setPassword(e.target.value)}
+          placeholder="Password" className="input input-bordered w-full max-w-xs my-2" />
+
+          </div>
+          <div className=" card-actions flex justify-center  ">
+            <button  onClick={handleLogin}
+            className="btn btn-primary ">Login</button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
